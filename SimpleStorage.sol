b15c7b24501dd5918d03b7690c8d7178cbf1076c
anmol-singh7 <singhanmol0984@gmail.com>
2024-12-28 11:32:12 +0530

    learn array and struct

--- SimpleStorage.sol
+++ SimpleStorage.sol
@@ -2,15 +2,32 @@
 pragma solidity 0.8.18;
 
 contract SimpleStorage {
-       uint256 public favoriteNumber;
+       uint256 public myFavoriteNumber;
 
        function store(uint256 _favoriteNumber) public {
-            favoriteNumber = _favoriteNumber;
+            myFavoriteNumber = _favoriteNumber;
             
        }
+
+       uint256[] listOfFavoriteNumber;
+
+
+       struct Person {
+            uint256 favoriteNumber;
+            string name;
+       }
+
+       Person public pat = Person(5,"pat");
+
+       Person public pat2 = Person({favoriteNumber:55, name:"pat2"});
        
+       Person[] public listOfPeople; // dynamic list
+       Person[3] public staticListOfPeople;
+
        // view, pure
        function retriever() public view returns(uint256) {
-           return favoriteNumber;
+           return myFavoriteNumber;
       }
+
+      
 }
