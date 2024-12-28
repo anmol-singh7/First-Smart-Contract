75572e338f32466ec92e9763dded84595b3270c7
anmol-singh7 <singhanmol0984@gmail.com>
2024-12-28 13:31:44 +0530

    mapping

--- SimpleStorage.sol
+++ SimpleStorage.sol
@@ -2,39 +2,42 @@
 pragma solidity 0.8.18;
 
 contract SimpleStorage {
-       uint256 public myFavoriteNumber;
+       uint256 public myFavouriteNumber;
+       
+       mapping(string => uint256) public nameToFavouriteNumber;
 
-       function store(uint256 _favoriteNumber) public {
-            myFavoriteNumber = _favoriteNumber;
+       function store(uint256 _favouriteNumber) public {
+            myFavouriteNumber = _favouriteNumber;
             
        }
 
-       uint256[] listOfFavoriteNumber;
+       uint256[] listOfFavouriteNumber;
 
 
        struct Person {
-            uint256 favoriteNumber;
+            uint256 favouriteNumber;
             string name;
        }
 
        Person public pat = Person(5,"pat");
 
-       Person public pat2 = Person({favoriteNumber:55, name:"pat2"});
+       Person public pat2 = Person({favouriteNumber:55, name:"pat2"});
        
        Person[] public listOfPeople; // dynamic list
        Person[3] public staticListOfPeople;
 
        // view, pure
        function retriever() public view returns(uint256) {
-           return myFavoriteNumber;
+           return myFavouriteNumber;
       }
       
       // calldata, memeory, storage
-      function addPerson(string memory _name, uint256 _favoriteNumber) public {
-           Person memory newPerson = Person(_favoriteNumber, _name);
+      function addPerson(string memory _name, uint256 _favouriteNumber) public {
+           Person memory newPerson = Person(_favouriteNumber, _name);
            listOfPeople.push(newPerson);
            //OR
-           // listOfPeople.push( Person(_favoriteNumber, _name));
+           // listOfPeople.push( Person(_favouriteNumber, _name));
+           nameToFavouriteNumber[_name] = _favouriteNumber;
       }
 
 
